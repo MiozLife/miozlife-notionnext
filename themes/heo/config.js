41c8b5e7d1de60677762cdb2def81dcb027d5759
MiozLife <mioz.life@outlook.com>
2026-04-02 16:57:53 +0900

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-01-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '如果内容显示不完全或显示错误，请多刷新几次', url: 'https://mioz.life' },
+    { title: '你好啊~', url: 'https://mioz.life' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '猫酣 | Mioz',
+  HEO_HERO_TITLE_2: '/ᐠ.ꞈ.ᐟ\∫',
+  HEO_HERO_TITLE_3: 'MIOZ.LIFE',
+  HEO_HERO_TITLE_4: '欢迎~',
+  HEO_HERO_TITLE_5: '猫酣 | Mioz',
+  HEO_HERO_TITLE_LINK: 'https://mioz.life',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '🧩 周记月记或年记', url: 'https://mioz.life/tag/summary' },
+  HEO_HERO_CATEGORY_2: { title: '🍵 来Cafe留言吧', url: '/cafe' },
+  HEO_HERO_CATEGORY_3: { title: '🐈 随机逛逛朋友圈', url: 'https://mioz.life/random#_blank' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
